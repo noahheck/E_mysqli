416bbc331f34d3a2215aaaca106eced995bc37d0
Noah Heck <myesain84@gmail.com>
2018-04-12 19:36:02 -0600

    Fix back reference syntax issue

--- src/EMysqliStmt.php
+++ src/EMysqliStmt.php
@@ -22,156 +22,167 @@ use mysqli_stmt;
 
 class EMysqliStmt extends mysqli_stmt
 {
-	/**
-	 * @var EMysqli
-	 */
-	private $mysqli;
+    /**
+     * @var EMysqli
+     */
+    private $mysqli;
 
-	/**
-	 * @var string - the unchanged query string provided to the constructor
-	 */
-	private $queryString;
+    /**
+     * @var string - the unchanged query string provided to the constructor
+     */
+    private $queryString;
 
-	/**
-	 * @var array - array of arrays containing values that have been bound to the query as parameters
-	 */
-	private $boundParams = array();
+    /**
+     * @var array - array of arrays containing values that have been bound to the query as parameters
+     */
+    private $boundParams = array();
 
-	/**
-	 * @var string - the query string with bound parameters interpolated
-	 */
-	public $fullQuery;
+    /**
+     * @var string - the query string with bound parameters interpolated
+     */
+    public $fullQuery;
 
-	/**
-	 * The first argument should be an instance of mysqli or descendent. If so, we'll cache it's reference locally to be
-	 * able to escape the parameters later. Also, we accept and store the query string.
-	 * @param mysqli $mysqli
-	 * @param string $query
-	 */
-	public function __construct(mysqli $mysqli, $query)
-	{
-		$this->mysqli      = $mysqli;
-		$this->queryString = $query;
+    /**
+     * The first argument should be an instance of mysqli or descendent. If so, we'll cache it's reference locally to be
+     * able to escape the parameters later. Also, we accept and store the query string.
+     * @param mysqli $mysqli
+     * @param string $query
+     */
+    public function __construct(mysqli $mysqli, $query)
+    {
+        $this->mysqli      = $mysqli;
+        $this->queryString = $query;
 
-		parent::__construct($mysqli, $query);
-	}
+        parent::__construct($mysqli, $query);
+    }
 
-	/**
-	 * We overwrite the default bind_param method so we can cache the parameters for our needs later. We also extend
-	 * the method to allow binding parameters
-	 *
-	 * @param string $type
-	 * @param mixed $vars
+    /**
+     * We overwrite the default bind_param method so we can cache the parameters for our needs later. We also extend
+     * the method to allow binding parameters
+     *
+     * @param string $type
+     * @param mixed $vars
      *
      * @return bool
-	 */
-	public function bind_param($type, &...$vars)
-	{
-		if ($vars) {
-			$types = str_split($type);
-			$arg   = 0;
+     */
+    public function bind_param($type, &...$vars)
+    {
+        if ($vars) {
+            $types = str_split($type);
+            $arg   = 0;
 
-			foreach ($types as $type) {
+            foreach ($types as $type) {
 
-				$this->boundParams[] = [
-					"type"  => $type,
-					"value" => &$vars[$arg],
-				];
+                $this->boundParams[] = [
+                    "type"  => $type,
+                    "value" => &$vars[$arg],
+                ];
 
-				$arg++;
-			}
-		}
+                $arg++;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * We interpolate our values into the fullQuery variable, then call the parent::bind_param so the values are
-	 * actually sent to the db server. Then we execute the query statement.
-	 *
-	 * @return bool
-	 */
-	public function execute()
-	{
-		$this->interpolateQuery();
+    /**
+     * We interpolate our values into the fullQuery variable, then call the parent::bind_param so the values are
+     * actually sent to the db server. Then we execute the query statement.
+     *
+     * @return bool
+     */
+    public function execute()
+    {
+        $this->interpolateQuery();
 
-		$params = $this->buildArguments();
+        $params = $this->buildArguments();
 
-		if (count($params) > 1) {
-			call_user_func_array(array('parent', "bind_param"), $params);
-		}
+        if (count($params) > 1) {
+            call_user_func_array(array('parent', "bind_param"), $params);
+        }
 
-		return parent::execute();
-	}
+        return parent::execute();
+    }
 
-	/**
-	 * Copies $this->queryString then replaces bound markers with associated values ($this->queryString is not modified
-	 * but the resulting query string is assigned to $this->fullQuery)
-	 *
-	 * The regex here was modified from this StackOverflow answer: http://stackoverflow.com/a/632552/2422852
-	 *
-	 * @return string $testQuery - interpolated db query string
-	 */
-	public function interpolateQuery()
-	{
-		$testQuery 	= $this->queryString;
+    /**
+     * Copies $this->queryString then replaces bound markers with associated values ($this->queryString is not modified
+     * but the resulting query string is assigned to $this->fullQuery)
+     *
+     * The regex here was modified from this StackOverflow answer: http://stackoverflow.com/a/632552/2422852
+     *
+     * @return string $testQuery - interpolated db query string
+     */
+    public function interpolateQuery()
+    {
+        $testQuery 	= $this->queryString;
 
-		if ($this->boundParams) {
+        // Back references may be replaced in the resultant interpolatedQuery, so we need to sanitize that syntax -
+        // This is the character map to sanitize the back reference syntax
+        $cleanBackRefCharMap = ['%'=>'%%', '$'=>'$%', '\\'=>'\\%'];
 
-			foreach ($this->boundParams as $key => $param) {
-				$type  = $param['type'];
-				$value = $param['value'];
+        $pattern = "/(\?)(?=(?:[^\"']|[\"'][^\"']*[\"'])*$)/";
 
-				$replValue = $this->prepareValue($value, $type);
-				$testQuery = preg_replace("/(\?)(?=(?:[^\"']|[\"'][^\"']*[\"'])*$)/", $replValue, $testQuery, 1);
-			}
-		}
+        if ($this->boundParams) {
 
-		$this->fullQuery = $testQuery;
+            foreach ($this->boundParams as $key => $param) {
+                $type  = $param['type'];
+                $value = $param['value'];
 
-		return $testQuery;
-	}
+                $replValue = $this->prepareValue($value, $type);
+
+                $backReferenceSafeReplValue = strtr($replValue, $cleanBackRefCharMap);
+
+                $testQuery = preg_replace($pattern, $backReferenceSafeReplValue, $testQuery, 1);
+            }
+        }
+
+        $testQuery = strtr($testQuery, array_flip($cleanBackRefCharMap));
+
+        $this->fullQuery = $testQuery;
+
+        return $testQuery;
+    }
 
 
 
-	/**
-	 * Combines the values stored in $this->boundParams into one array suitable for pushing as the input arguments to
-	 * parent::bind_param when used with call_user_func_array
-	 * @return array
-	 */
-	private function buildArguments()
-	{
-		$arguments    = array();
-		$arguments[0] = "";
+    /**
+     * Combines the values stored in $this->boundParams into one array suitable for pushing as the input arguments to
+     * parent::bind_param when used with call_user_func_array
+     * @return array
+     */
+    private function buildArguments()
+    {
+        $arguments    = array();
+        $arguments[0] = "";
 
-		foreach ($this->boundParams as $param) {
-			$arguments[0] .= $param['type'];
-			$arguments[]   = &$param['value'];
-		}
+        foreach ($this->boundParams as $param) {
+            $arguments[0] .= $param['type'];
+            $arguments[]   = &$param['value'];
+        }
 
-		return $arguments;
-	}
+        return $arguments;
+    }
 
-	/**
-	 * Escapes the supplied value according to mysqli::real_escape_string which we should have cached a reference to an
-	 * active connection at construct. The result of that is wrapped in apostrophes as well if the bound parameter is
-	 * not identified as an integer.
-	 *
-	 * @param mixed $value
-	 * @param string $type (one of 'i', 'b', 's', 'd')
-	 * @return string $value escaped for insertion into the interpolated query string
-	 */
-	private function prepareValue($value, $type)
-	{
-		if ($value === NULL) {
-			return 'NULL';
-		}
+    /**
+     * Escapes the supplied value according to mysqli::real_escape_string which we should have cached a reference to an
+     * active connection at construct. The result of that is wrapped in apostrophes as well if the bound parameter is
+     * not identified as an integer.
+     *
+     * @param mixed $value
+     * @param string $type (one of 'i', 'b', 's', 'd')
+     * @return string $value escaped for insertion into the interpolated query string
+     */
+    private function prepareValue($value, $type)
+    {
+        if ($value === NULL) {
+            return 'NULL';
+        }
 
-		if ('i' === $type) {
-			return (int) $value;
-		}
+        if ('i' === $type) {
+            return (int) $value;
+        }
 
-		return "'" . $this->mysqli->real_escape_string($value) . "'";
-	}
+        return "'" . $this->mysqli->real_escape_string($value) . "'";
+    }
 }
 
--- tests/src/EMysqliStmtTest.php
+++ tests/src/EMysqliStmtTest.php
@@ -23,279 +23,294 @@ use EMysqli\EMysqli;
 
 class EMysqliStmtTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var EMysqli
-	 */
-	protected $eMysqli;
+    /**
+     * @var EMysqli
+     */
+    protected $eMysqli;
 
-	/**
-	 * @return array
-	 */
-	protected function getConfig()
-	{
-		return require dirname(__FILE__) . "/../config/config.php";
-	}
+    /**
+     * @return array
+     */
+    protected function getConfig()
+    {
+        return require dirname(__FILE__) . "/../config/config.php";
+    }
 
-	/**
-	 * Returns an instance of the EMysqli object - if this is the first time asking for it, a new instance is created
-	 * and cached to be returned on subsequent calls
-	 *
-	 * @return EMysqli
-	 */
-	protected function getEMysqli()
-	{
-		if ($this->eMysqli) {
-			return $this->eMysqli;
-		}
+    /**
+     * Returns an instance of the EMysqli object - if this is the first time asking for it, a new instance is created
+     * and cached to be returned on subsequent calls
+     *
+     * @return EMysqli
+     */
+    protected function getEMysqli()
+    {
+        if ($this->eMysqli) {
+            return $this->eMysqli;
+        }
 
-		$config = $this->getConfig();
+        $config = $this->getConfig();
 
-		$host     = ($config['db']['host'])     ? $config['db']['host']     : null;
-		$username = ($config['db']['username']) ? $config['db']['username'] : null;
-		$password = ($config['db']['password']) ? $config['db']['password'] : null;
-		$dbname   = ($config['db']['dbname'])   ? $config['db']['dbname']   : null;
-		$port     = ($config['db']['port'])     ? $config['db']['port']     : null;
-		$socket   = ($config['db']['socket'])   ? $config['db']['socket']   : null;
+        $host     = ($config['db']['host'])     ? $config['db']['host']     : null;
+        $username = ($config['db']['username']) ? $config['db']['username'] : null;
+        $password = ($config['db']['password']) ? $config['db']['password'] : null;
+        $dbname   = ($config['db']['dbname'])   ? $config['db']['dbname']   : null;
+        $port     = ($config['db']['port'])     ? $config['db']['port']     : null;
+        $socket   = ($config['db']['socket'])   ? $config['db']['socket']   : null;
 
-		$this->eMysqli = new EMysqli($host, $username, $password, $dbname, $port, $socket);
+        $this->eMysqli = new EMysqli($host, $username, $password, $dbname, $port, $socket);
 
-		return $this->eMysqli;
-	}
+        return $this->eMysqli;
+    }
 
-	protected function getPreparedStatement($query)
-	{
-		$db = $this->getEMysqli();
+    protected function getPreparedStatement($query)
+    {
+        $db = $this->getEMysqli();
 
-		return $db->prepare($query);
-	}
+        return $db->prepare($query);
+    }
 
 
 
-	public function testQuotedStringValuesGetInterpolatedIntoQueryWhenBoundAsAGroup()
-	{
-		$query = "INSERT INTO contacts SET first_name = ?, last_name = ?";
+    public function testQuotedStringValuesGetInterpolatedIntoQueryWhenBoundAsAGroup()
+    {
+        $query = "INSERT INTO contacts SET first_name = ?, last_name = ?";
 
-		$firstName = "Noah";
-		$lastName  = "Heck";
+        $firstName = "Noah";
+        $lastName  = "Heck";
 
-		$expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
+        $expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("ss", $firstName, $lastName);
+        $stmt->bind_param("ss", $firstName, $lastName);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testQuotedStringValuesGetInterpolatedIntoQueryWhenBoundIndividually()
-	{
-		$query = "INSERT INTO contacts SET first_name = ?, last_name = ?";
+    public function testQuotedStringValuesGetInterpolatedIntoQueryWhenBoundIndividually()
+    {
+        $query = "INSERT INTO contacts SET first_name = ?, last_name = ?";
 
-		$firstName = "Noah";
-		$lastName  = "Heck";
+        $firstName = "Noah";
+        $lastName  = "Heck";
 
-		$expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
+        $expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("s", $firstName);
-		$stmt->bind_param("s", $lastName);
+        $stmt->bind_param("s", $firstName);
+        $stmt->bind_param("s", $lastName);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testUnquotedIntegerValuesGetInterpolatedIntoQueryWhenBoundAsAGroup()
-	{
-		$query = "SELECT * FROM contacts WHERE contacts_id IN (?, ?)";
+    public function testUnquotedIntegerValuesGetInterpolatedIntoQueryWhenBoundAsAGroup()
+    {
+        $query = "SELECT * FROM contacts WHERE contacts_id IN (?, ?)";
 
-		$contactId1 = 1;
-		$contactId2 = 2;
+        $contactId1 = 1;
+        $contactId2 = 2;
 
-		$expected = "SELECT * FROM contacts WHERE contacts_id IN (1, 2)";
+        $expected = "SELECT * FROM contacts WHERE contacts_id IN (1, 2)";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("ii", $contactId1, $contactId2);
+        $stmt->bind_param("ii", $contactId1, $contactId2);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testUnquotedIntegerValuesGetInterpolatedIntoQueryWhenBoundIndividually()
-	{
-		$query = "SELECT * FROM contacts WHERE contacts_id IN (?, ?)";
+    public function testUnquotedIntegerValuesGetInterpolatedIntoQueryWhenBoundIndividually()
+    {
+        $query = "SELECT * FROM contacts WHERE contacts_id IN (?, ?)";
 
-		$contactId1 = 1;
-		$contactId2 = 2;
+        $contactId1 = 1;
+        $contactId2 = 2;
 
-		$expected = "SELECT * FROM contacts WHERE contacts_id IN (1, 2)";
+        $expected = "SELECT * FROM contacts WHERE contacts_id IN (1, 2)";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("i", $contactId1);
-		$stmt->bind_param("i", $contactId2);
+        $stmt->bind_param("i", $contactId1);
+        $stmt->bind_param("i", $contactId2);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testValuesGetInterpolatedIntoQueryAndQuotedCorrectlyWhenMixedArgumentsBoundAsAGroup()
-	{
-		$query = "SELECT * FROM contacts WHERE contacts_id = ? OR first_name = ?";
+    public function testValuesGetInterpolatedIntoQueryAndQuotedCorrectlyWhenMixedArgumentsBoundAsAGroup()
+    {
+        $query = "SELECT * FROM contacts WHERE contacts_id = ? OR first_name = ?";
 
-		$contactId = 1;
-		$firstName = "Noah";
+        $contactId = 1;
+        $firstName = "Noah";
 
-		$expected = "SELECT * FROM contacts WHERE contacts_id = 1 OR first_name = 'Noah'";
+        $expected = "SELECT * FROM contacts WHERE contacts_id = 1 OR first_name = 'Noah'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("is", $contactId, $firstName);
+        $stmt->bind_param("is", $contactId, $firstName);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testValuesGetInterpolatedIntoQueryAndQuotedCorrectlyWhenMixedArgumentsBoundIndividually()
-	{
-		$query = "SELECT * FROM contacts WHERE contacts_id = ? OR first_name = ?";
+    public function testValuesGetInterpolatedIntoQueryAndQuotedCorrectlyWhenMixedArgumentsBoundIndividually()
+    {
+        $query = "SELECT * FROM contacts WHERE contacts_id = ? OR first_name = ?";
 
-		$contactId = 1;
-		$firstName = "Noah";
+        $contactId = 1;
+        $firstName = "Noah";
 
-		$expected = "SELECT * FROM contacts WHERE contacts_id = 1 OR first_name = 'Noah'";
+        $expected = "SELECT * FROM contacts WHERE contacts_id = 1 OR first_name = 'Noah'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("i", $contactId);
-		$stmt->bind_param("s", $firstName);
+        $stmt->bind_param("i", $contactId);
+        $stmt->bind_param("s", $firstName);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testInterpolationAllowsSuccessfulExecutionOfQueries()
-	{
-		$query = "SELECT ? + ? + ?, ?";
+    public function testInterpolationAllowsSuccessfulExecutionOfQueries()
+    {
+        $query = "SELECT ? + ? + ?, ?";
 
-		$int1   = 1;
-		$int2   = 1;
-		$int3   = 1;
-		$string = "Some String";
+        $int1   = 1;
+        $int2   = 1;
+        $int3   = 1;
+        $string = "Some String";
 
-		$expected = "SELECT 1 + 1 + 1, 'Some String'";
+        $expected = "SELECT 1 + 1 + 1, 'Some String'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("iiis", $int1, $int2, $int3, $string);
+        $stmt->bind_param("iiis", $int1, $int2, $int3, $string);
 
-		$stmt->execute();
+        $stmt->execute();
 
-		$this->assertEquals($expected, $stmt->fullQuery);
+        $this->assertEquals($expected, $stmt->fullQuery);
 
-		$stmt->bind_result($three, $someString);
+        $stmt->bind_result($three, $someString);
 
-		$stmt->fetch();
+        $stmt->fetch();
 
-		$this->assertEquals("3", $three);
-		$this->assertEquals($string, $someString);
-	}
+        $this->assertEquals("3", $three);
+        $this->assertEquals($string, $someString);
+    }
 
 
 
-	public function testInterpolationSucceedsEvenWhenReplacementValueContainsAPlaceholderCharacter()
-	{
-		$query = "INSERT INTO notes SET note = ?, contact_id = ?";
+    public function testInterpolationSucceedsEvenWhenReplacementValueContainsAPlaceholderCharacter()
+    {
+        $query = "INSERT INTO notes SET note = ?, contact_id = ?";
 
-		$note      = "Some string that contains a ?";
-		$contactId = 1;
+        $note      = "Some string that contains a ?";
+        $contactId = 1;
 
-		$expected = "INSERT INTO notes SET note = 'Some string that contains a ?', contact_id = 1";
+        $expected = "INSERT INTO notes SET note = 'Some string that contains a ?', contact_id = 1";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("si", $note, $contactId);
+        $stmt->bind_param("si", $note, $contactId);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
 
 
-	public function testInterpolationSucceedsEvenWhenReplacementValueContainsAPlaceholderCharacterAndQuerySpansMultipleLines()
-	{
-		$query = "INSERT INTO notes SET note = ?, contact_id = ?";
+    public function testInterpolationSucceedsEvenWhenReplacementValueContainsAPlaceholderCharacterAndQuerySpansMultipleLines()
+    {
+        $query = "INSERT INTO notes SET note = ?, contact_id = ?";
 
-		$note      = "Some string that contains a ?
-						or two ? and spans multiple lines";
-		$contactId = 1;
+        $note      = "Some string that contains a ?
+                        or two ? and spans multiple lines";
+        $contactId = 1;
 
-		$expected = "INSERT INTO notes SET note = 'Some string that contains a ?
-						or two ? and spans multiple lines', contact_id = 1";
+        $expected = "INSERT INTO notes SET note = 'Some string that contains a ?
+                        or two ? and spans multiple lines', contact_id = 1";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("si", $note, $contactId);
+        $stmt->bind_param("si", $note, $contactId);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		/**
-		 * The str_replace is here because mysqli::real_escape_string escapes \n characters as well, and this makes the
-		 * test more readable (?)
-		 */
-		$this->assertEquals($expected, str_replace("\\n", "\n", $result));
-	}
+        /**
+         * The str_replace is here because mysqli::real_escape_string escapes \n characters as well, and this makes the
+         * test more readable (?)
+         */
+        $this->assertEquals($expected, str_replace("\\n", "\n", $result));
+    }
 
 
 
-	public function testQueryIsNotChangedIfNoParametersUsedInQuery()
-	{
-		$query = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
+    public function testQueryIsNotChangedIfNoParametersUsedInQuery()
+    {
+        $query = "INSERT INTO contacts SET first_name = 'Noah', last_name = 'Heck'";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$this->assertEquals($query, $stmt->interpolateQuery());
-	}
+        $this->assertEquals($query, $stmt->interpolateQuery());
+    }
 
 
 
-	public function testNullValuesAreInterpolatedIntoQuerySuccessfullyAsDBNullValues()
-	{
-		$query = "INSERT INTO contacts SET first_name = ?, last_name = ?, email = ?";
+    public function testNullValuesAreInterpolatedIntoQuerySuccessfullyAsDBNullValues()
+    {
+        $query = "INSERT INTO contacts SET first_name = ?, last_name = ?, email = ?";
 
-		$firstName = "Noah";
-		$lastName  = null;
-		$email     = "";
+        $firstName = "Noah";
+        $lastName  = null;
+        $email     = "";
 
-		$expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = NULL, email = ''";
+        $expected = "INSERT INTO contacts SET first_name = 'Noah', last_name = NULL, email = ''";
 
-		$stmt = $this->getPreparedStatement($query);
+        $stmt = $this->getPreparedStatement($query);
 
-		$stmt->bind_param("sss", $firstName, $lastName, $email);
+        $stmt->bind_param("sss", $firstName, $lastName, $email);
 
-		$result = $stmt->interpolateQuery();
+        $result = $stmt->interpolateQuery();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDollarSignBackReferenceSyntaxGetsOutputCorrectlyInFullQuery()
+    {
+        $hashedPassword = '$2y$10$yOwqQMxRo0AveSZ6I6Yhn.aMqbtGYrKQvcLGEtanplhdboUM1ffGi';
+
+        $query = "UPDATE users SET password = ?";
+
+        $stmt = $this->getPreparedStatement($query);
+
+        $stmt->bind_param("s", $hashedPassword);
+
+        $result = $stmt->interpolateQuery();
+
+        $this->assertEquals("UPDATE users SET password = '$hashedPassword'", $result);
+    }
 }
